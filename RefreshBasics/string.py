55396e32a610dfafe314338bb2ee8272be793e22
StarcraftSu <starcraftsu@gmail.com>
2023-10-01 11:02:26 +0200

    string and ask for input

---
+++ RefreshBasics/string.py
@@ -0,0 +1,9 @@
+name = 'Starcraft'
+print(f'Hello, {name}')
+
+name = 'Bob'
+
+greeting = 'Hello, {}'
+with_name = greeting.format(name)
+
+print(with_name)
---
+++ RefreshBasics/userInput.py
@@ -0,0 +1,3 @@
+size = float(input('How big is your house?'))
+square_feet = size * 10.8
+print(f'{size} square meters is {square_feet:.2f} square feet')
